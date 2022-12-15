8609a26d18d9bc6b14720751ca21e73c56e7c056
Raphael Stock <raphael@stocks-web.de>
2022-12-15 18:24:03 +0100

    Add scaling factor to exponential fit
    Change-Id: I2e8d4ff243f6b8c7ef70fd21af5a822b5aace174

--- src/py/model_hw_mc_genetic/compartment_chain.py
+++ src/py/model_hw_mc_genetic/compartment_chain.py
@@ -241,6 +241,41 @@ def extract_psp_heights(
     return np.asarray(heights)
 
 
+def fit_exponential(heights: np.ndarray) -> float:
+    '''
+    Fit an exponential decay to the PSP height in the given array.
+
+    :param heights: PSP heights to which an exponential should be fitted.
+    :return: All fit parameters of the exponential fit (tau, offset,
+        scaling_factor).
+    '''
+    compartments = np.arange(len(heights))
+
+    def fitfunc(location, tau, offset, scaling_factor):
+        return scaling_factor * np.exp(- location / tau) + offset
+
+    # initial guess for fit parameters
+    guessed_tau = np.argmin(np.abs((heights - heights[-1])
+                                   - (heights[0] - heights[-1]) / np.e))
+    p_0 = {'tau': guessed_tau, 'offset': heights[-1],
+           'scaling_factor': heights[0] - heights[-1]}
+    bounds = {'tau': [0, np.inf],
+              'offset': [-np.inf, np.inf],
+              'scaling_factor': [0, np.inf]}
+
+    popt = curve_fit(
+        fitfunc,
+        compartments,
+        heights,
+        p0=[p_0['tau'], p_0['offset'], p_0['scaling_factor']],
+        bounds=([bounds['tau'][0], bounds['offset'][0],
+                 bounds['scaling_factor'][0]],
+                [bounds['tau'][1], bounds['offset'][1],
+                 bounds['scaling_factor'][1]]))[0]
+
+    return popt
+
+
 def fit_length_constant(heights: np.ndarray) -> float:
     '''
     Fit an exponential decay to the PSP height in the given array.
@@ -249,25 +284,4 @@ def fit_length_constant(heights: np.ndarray) -> float:
     :return: Length constant (exponential fit to PSP height as function of
         compartment)
     '''
-    norm_height = heights / heights[0]
-    compartments = np.arange(len(heights))
-
-    def fitfunc(location, tau, offset):
-        return np.exp(- location / tau) + offset
-
-    # initial guess for fit parameters
-    guessed_tau = len(heights) if norm_height[-1] > 1 / np.e else \
-        compartments[np.argmin(norm_height > 1 / np.e)]
-    p_0 = {'tau': guessed_tau, 'offset': norm_height[-1]}
-    bounds = {'tau': [0, np.inf],
-              'offset': p_0['offset'] + np.array([-1, 1])}
-
-    popt = curve_fit(
-        fitfunc,
-        compartments,
-        norm_height,
-        p0=[p_0['tau'], p_0['offset']],
-        bounds=([bounds['tau'][0], bounds['offset'][0]],
-                [bounds['tau'][1], bounds['offset'][1]]))[0]
-
-    return popt[0]
+    return fit_exponential(heights)[0]
---
+++ tests/sw/py/model_hw_mc_genetic/test_length_const_fit.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+import unittest
+from itertools import product
+
+import numpy as np
+
+from model_hw_mc_genetic.compartment_chain import fit_exponential
+
+
+class TestFitLengthConstant(unittest.TestCase):
+    @staticmethod
+    def _get_data(location, tau, offset, scaling_factor, sigma):
+        def fitfunc(location, tau, offset, scaling_factor):
+            return scaling_factor * np.exp(- location / tau) + offset
+        # sample from exponential
+        height = fitfunc(location, tau, offset, scaling_factor)
+        # add Gaussian noise
+        height += np.random.normal(0, sigma, height.shape)
+        return height
+
+    def test_typical_madc_ranges(self):
+        '''
+        Test that fitted parameters agree with original parameters.
+
+        We generate dummy data for different taus, scaling factors and offsets.
+        Then we fit an exponential to this data and assert that we get the
+        original parameters back.
+        '''
+
+        offsets = np.array([-5, 400])
+        scaling_factors = np.array([100, 200])
+        taus = np.array([0.75, 1.25, 1.75])
+
+        for target_params in product(taus, offsets, scaling_factors):
+            comps = np.arange(5)
+            height = self._get_data(comps, *target_params, 0)
+            fitted_params = fit_exponential(height)
+            for target, fitted in zip(target_params, fitted_params):
+                self.assertAlmostEqual(target, fitted)
+
+    def test_with_noise(self):
+        '''
+        Test that fit is running when noise is added to the generated data.
+
+        We generate dummy data for different taus, scaling factors and offsets.
+        To this data noise is added and an exponential is fitted.
+        We just test that the fitting does not throw any errors. We do not
+        evaluate the quality of the fit.
+
+        This aims to ensure that the fit routine can deal with the typical
+        noise encountered during experiments.
+        '''
+
+        offsets = np.array([-5, 400])
+        scaling_factors = np.array([100, 200])
+        taus = np.array([0.75, 1.25, 1.75])
+
+        # The normal noise level of the resting membrane is approximately
+        # 3 LSB for MADC measurements.
+        noise = 3
+
+        for target_params in product(taus, offsets, scaling_factors):
+            comps = np.arange(5)
+            heights = self._get_data(comps, *target_params, noise)
+            fit_exponential(heights)
+
+
+if __name__ == "__main__":
+    unittest.main()
