92c1c809fafce2b9a3e47de56ac0a6edd232d9df
Jakob Kaiser <jakob.kaiser@kip.uni-heidelberg.de>
2023-02-06 15:40:06 +0000

    Remove old dummy sw test
    Change-Id: I8fa902f4ffd71e8ffbdf36e363a12ac8cca4fe33

--- src/py/model_hw_mc_genetic/__init__.py
+++ src/py/model_hw_mc_genetic/__init__.py
@@ -9,13 +9,3 @@ def get_neuron_population(size: int) -> pynn.Population:
     :return: Neuron population
     """
     return pynn.Population(size, pynn.cells.HXNeuron())
-
-
-def add(val_a: int, val_b: int) -> int:
-    """
-    Special implementation of a number adder.
-    :param val_a: First addition argument
-    :param val_b: Second addition argument
-    :return: Addition result
-    """
-    return val_a + val_b
--- tests/sw/py/model_hw_mc_genetic/test_adder.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-from model_hw_mc_genetic import add
-
-
-class TestAdder(unittest.TestCase):
-    def test_commutative(self):
-        self.assertEqual(add(3, 4), add(4, 3))
