1580e5be32d4476496342bbfab204a369a7d172c
Jakob Kaiser <jakob.kaiser@kip.uni-heidelberg.de>
2023-03-20 14:07:28 +0000

    Remove minimal example from template
    Change-Id: Ie731dd1d6ae0ba928d1ae2746ba9c302d023b991

--- tests/hw/py/model_hw_mc_genetic/test_minimal_experiment.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-from model_hw_mc_genetic.scripts.minimal_experiment import main
-
-
-class TestMinimalExperiment(unittest.TestCase):
-    def test_experiment(self):
-        self.assertIsNone(main())
